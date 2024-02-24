8905560775125e7753bd48cd7fbe24865f28e063
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 14:20:16 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/데이터 교환/exchange-data.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b  = 6, c = 7; 
+    int temp;
+    temp = b;
+    b = a;
+    a = c ;
+    c = temp;
+    cout << a <<endl << b << endl << c;// 여기에 코드를 작성해주세요.
+    return 0;
+}
