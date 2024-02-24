cee00a0acb8da41475e5f135a1a9fe7095c25c66
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 14:02:00 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float a = 5.26;
+    float b = 8.27;
+    cout.precision(3);
+    cout << fixed;
+    cout<< a*b ;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
