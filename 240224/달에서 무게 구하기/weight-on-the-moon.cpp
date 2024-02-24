563071d436db6e62e14da81516e9d5a640140134
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 12:48:42 +0900

    Title: 달에서 무게 구하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int chu = 13;
+    float gravity = 0.165000;
+    cout.precision(6);
+    cout<<fixed;
+    cout << "13 * 0.165000 = " << chu*gravity;// 여기에 코드를 작성해주세요.
+    return 0;
+}
