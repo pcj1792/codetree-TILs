8128bfbab4ec457ffae9f57972c0c138716d0633
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 15:14:25 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int h, m;
+    cin >> h;
+    cin.get();
+    cin >> m;
+    cout << h +1 <<":" <<m;// 여기에 코드를 작성해주세요.
+    return 0;
+}
