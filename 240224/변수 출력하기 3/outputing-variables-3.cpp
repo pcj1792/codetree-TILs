2d2e5d57d5e4d3151930273f5c76005c4ef5a1e7
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 12:44:02 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int a = 1;
+    int b = 2;
+    string c = "C";// 여기에 코드를 작성해주세요.
+    cout<< a << "->" <<b <<"->"<<c;
+    return 0;
+}
