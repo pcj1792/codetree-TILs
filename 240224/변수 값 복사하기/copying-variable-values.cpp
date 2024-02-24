65c2d8d068fac6bb0bef21a72e5bc57d4bacface
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 14:22:53 +0900

    Title: 변수 값 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240224/변수 값 복사하기/copying-variable-values.cpp
+++ 240224/변수 값 복사하기/copying-variable-values.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a = 1, b = 2, c = 3;
     a = b =c ;
-    cout << a << b << c ;// 여기에 코드를 작성해주세요.
+    cout << a << " " <<b << " "<< c ;// 여기에 코드를 작성해주세요.
     return 0;
 }
