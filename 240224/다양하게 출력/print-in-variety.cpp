c2a3a732e100fa4e0f8ee9b899037e12ad4ce9ce
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 12:22:55 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    //cout.precision(12);
+    //cout<<fixed;
+    cout<< "Total days in Year" <<endl<< 365 <<endl <<"Circumference rate"
+    <<endl<<"3.1415926535";// 여기에 코드를 작성해주세요.
+    return 0;
+}
