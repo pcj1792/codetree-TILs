f6bfc689803d41b1fd688c089aa01f340dc3ec27
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 14:15:25 +0900

    Title: 변수 값 교체하기 6, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =2 ,b =5;
+    int temp;// 여기에 코드를 작성해주세요.
+    temp =a;
+    a = b;
+    b =temp;
+    cout << a <<endl << b;
+    return 0;
+}
