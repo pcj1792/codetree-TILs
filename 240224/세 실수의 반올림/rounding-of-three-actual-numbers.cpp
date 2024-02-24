8a103fe7fccf0e8ffe699170cc670e9abb2c51c5
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 15:01:56 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    double a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout.precision(3);
+    cout << fixed << a << endl << b << endl << c;// 여기에 코드를 작성해주세요.
+    return 0;
+}
