91ff620c370f8e40df764fa7131af8165549470e
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 13:59:02 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a =280.4;
+    float b = 209214.2;
+    cout.precision(7);
+    cout<< "9.2ft = " << a <<"cm"<<endl;
+    cout<< "1.3mi = " << b << "cm";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
