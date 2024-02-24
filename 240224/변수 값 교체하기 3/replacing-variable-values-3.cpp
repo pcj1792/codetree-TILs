3d9583d1e16b02cd9bf51178c96af9e6ef7c54e7
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 14:14:05 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 5;
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << a << endl <<b; // 여기에 코드를 작성해주세요.
+    return 0;
+}
