9a3cbd20ee3ee22a4597fe97654352aff468008e
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-24 14:45:56 +0900

    Title: 입력받은 값 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int temp;
+    cin >> a>> b;
+    temp = a;
+    a= b;
+    b = temp;
+    
+    cout << a << " " << b;// 여기에 코드를 작성해주세요.
+    return 0;
+}
