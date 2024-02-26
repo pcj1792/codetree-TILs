db7000c68ec3fd2a478d6ba2fdfac6054abdd27a
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-26 18:13:05 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int c = a+b;
+    int d = a-b;
+    int f = a/b;
+    int g = a%b;
+    cout << c <<endl << d <<endl << f << endl << g;// 여기에 코드를 작성해주세요.
+    return 0;
+}
