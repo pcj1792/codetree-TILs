d12e2c4f1eb4b46b4ac371c8dc7f91855500ba7a
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-26 19:08:18 +0900

    Title: 세 수의 합과 평균 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240226/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
+++ 240226/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
-    cout << a+b+c <<endl << a+b+c/3;// 여기에 코드를 작성해주세요.
+    cout << a+b+c <<endl << (a+b+c)/3;// 여기에 코드를 작성해주세요.
     return 0;
 }
