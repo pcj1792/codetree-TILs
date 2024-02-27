cd6c563fcf8f6fba2f203959beaf3b383b4c7332
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-27 11:30:21 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240227/살 수 있는 물건 2/things-able-to-buy-2.cpp
+++ 240227/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -5,7 +5,7 @@ int main() {
     cin >> n;
     if(n>=3000) cout << "book";
     else if(n>=1000) cout << "mask";
-    else if(n>=3000) cout << "pen";
+    else if(n>=500) cout << "pen";
     else cout << "no";
     // 여기에 코드를 작성해주세요.
     return 0;
