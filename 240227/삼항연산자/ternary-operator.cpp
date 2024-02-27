4774aee055dac37247c6bc733f0639ccd8048aef
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-27 10:36:27 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    string z = n==100? "pass" : "failure";
+    cout << z;// 여기에 코드를 작성해주세요.
+    return 0;
+}
