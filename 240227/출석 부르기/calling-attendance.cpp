5dd11171db9e84b58c07d8cf1ec976cebec3174e
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-27 11:15:20 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int John =1, Tom =2, Paul =3;
+    int a;
+    cin >> a;
+    if (a==1) cout << "John";
+    else if (a==2) cout << "Tom";
+    else if (a==3) cout << "Paul";
+    else cout << "Vaccancy";
+    return 0;
+}
