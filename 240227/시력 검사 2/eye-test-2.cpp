ab0ee3c91658498ce54659adc0b1a9aa7b938699
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-27 11:03:40 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240227/시력 검사 2/eye-test-2.cpp
+++ 240227/시력 검사 2/eye-test-2.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
-    float m  ;
+    double m  ;
     cin >> m;
     if( m >=1.0){
         cout << "High";
     }
-    else if (1.0> m >=0.5){
+    else if (m >=0.5 ){
         cout << "Middle";
     }
     
-    else cout << "Low";// 여기에 코드를 작성해주세요.
+    else cout << "Low";
+    // 여기에 코드를 작성해주세요.
     return 0;
 }
