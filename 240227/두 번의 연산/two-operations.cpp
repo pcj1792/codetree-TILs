c48e78d9391a5a88c49806b3b8a11839f880212a
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-27 14:07:50 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/두 번의 연산/two-operations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a%2==1) a=a+3 ;
+    if (a%3==0) a=a/3;
+    cout << a;// 여기에 코드를 작성해주세요.
+    return 0;
+}
