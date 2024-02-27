8632649fa4dc171a1e0f0252728eaf37e0666c04
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-27 10:01:57 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if(n >= 80){
+        cout << "pass";
+    }
+    else cout << endl << "more score";
+    return 0;
+}
