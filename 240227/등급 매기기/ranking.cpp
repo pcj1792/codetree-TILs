ae414f0e3f9d41634f478cf1ccbed3990f513fcb
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-27 11:18:43 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/등급 매기기/ranking.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a>=90) cout << "A";
+    else if (a>=80) cout << "B";
+    else if (a>=70) cout << "C";
+    else if (a>=60) cout << "D";
+    else  cout << "F";// 여기에 코드를 작성해주세요.
+    return 0;
+}
