c5c344ee7e8b612250b376560d64015640c75239
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-29 08:58:37 +0900

    Title: 숫자의 계절은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/숫자의 계절은/season-of-num.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int m;
+    cin >> m;
+    if(m==12 || m<=2) cout << "Winter";
+    else if(m>=9) cout << "Fall";
+    else if(m>=6) cout << "Summer";
+    else cout << "Spring";
+    return 0;
+}
