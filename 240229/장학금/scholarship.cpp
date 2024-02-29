8311debc001967861a1013646d8dfc1a3a93027d
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-29 09:08:54 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240229/장학금/scholarship.cpp
+++ 240229/장학금/scholarship.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     int m, f ; cin >> m >> f;
     if(m<90 || f<90) cout << 0;
-    else if(f>=95) cout << 10;
-    else cout << 5;
+    else if(f>=95) cout << 100000;
+    else cout << 50000;
     // 여기에 코드를 작성해주세요.
     return 0;
 }
