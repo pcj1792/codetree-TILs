f243f3a87426332b719025063687862848916963
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-02-29 09:41:20 +0900

    Title: 좀 더 어려운 수학 점수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240229/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 240229/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> bm >> be;
     if(am>bm) cout << "A";
     else if(am<bm) cout << "B";
-    else if(am==bm || ae > be) cout << "A";
+    else if(am==bm && ae > be) cout << "A";
     else cout << "B" ;// 여기에 코드를 작성해주세요.
     return 0;
 }
