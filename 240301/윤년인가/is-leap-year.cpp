59cea8bb20d5d67e0111cb4563ffde5dae93910a
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-01 12:03:38 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240301/윤년인가/is-leap-year.cpp
+++ 240301/윤년인가/is-leap-year.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int main() {
     int y;
     cin >> y;
-    string z= ((y%4==0 || y%400==0) || y%100==0 ) ? "true" : "false";
-    cout << z;
+    if(y%4==0){
+        if(y%400!=0){
+            cout << "false"; 
+        }
+    }
+    else cout << "true";
+    
     return 0;
 }
