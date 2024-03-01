4b89e6995288687845f254b4100853f94896e541
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-01 11:19:21 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240301/두 사람/two-person.cpp
+++ 240301/두 사람/two-person.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main() {
     int aa, ba;
-    char s;
-    cin >> aa >> s;
-    cin >> ba >> s;
-    cout << ((aa >= 19 || ba >= 19) || s=='M')? 1:0;
-
+    char as,bs;
+    cin >> aa >> as;
+    cin >> ba >> bs;
+     if(aa >=19 && as=='M') cout << 1;
+     else if(ba >=19 && bs=='M') cout << 1;
+     else cout <<0;
     return 0;
 }
