4a6d86bb5f124f85841794ad1ecb0c6ab70b2b43
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-01 13:02:12 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240301/최댓값 구하기/maximum-value.cpp
+++ 240301/최댓값 구하기/maximum-value.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a>> b >> c;
-    if((a>=b && a>=c)&&b>=c) cout << a;
-    else if( a<=c&&b<=c) cout << c;
+    if(a>=b &&  a>=c) cout << a;
+    else if( a<=c && b<=c) cout << c;
     else cout << b;
     return 0;
 }
