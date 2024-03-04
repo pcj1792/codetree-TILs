199452d3082029ce908d19df6fb33ba8241a5635
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-04 17:20:41 +0900

    Title: 정수 입력받아 배수 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n; 
+    cin >> n;
+    for(int i=1; i<=5; i++){
+        int a=n*i;
+        cout << a << " ";
+    }
+    return 0;
+}
