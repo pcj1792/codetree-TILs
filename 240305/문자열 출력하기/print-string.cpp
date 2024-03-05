e7bc059f49cdf28cf7b7dd7113fd2ecd53fba2a9
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-05 09:16:04 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/문자열 출력하기/print-string.cpp
+++ 240305/문자열 출력하기/print-string.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for(int i=1; i<=n; n++) cout << "LeebrosCode"<< endl;
+    for(int i=1; i<=n; i++) cout << "LeebrosCode"<< endl;
 
     return 0;
 }
