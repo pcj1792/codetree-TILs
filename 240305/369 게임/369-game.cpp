8c249cc27af9ca90973c87cc762c350564b0f612
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-05 19:07:51 +0900

    Title: 369 게임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/369 게임/369-game.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,i;
+    cin >> n;
+    for(i=1; i<=n; i++){
+        if(i%3==0) cout << 0 <<" ";
+        else if(i%10==3 || i%10==6 || i%10==9) cout << 0 <<" ";
+        else if(i/10==3 || i/10==6 || i/10==9) cout << 0 <<" ";
+        else cout << i <<" ";
+    }
+    return 0;
+}
