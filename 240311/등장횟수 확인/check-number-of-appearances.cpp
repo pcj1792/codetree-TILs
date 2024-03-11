ca287da0ecbf46e285a228384e42f3750d1ab8d5
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-11 09:16:01 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240311/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, cnt=0;
+    for(int i=1; i<=5; i++){
+        cin  >> n;
+        if(n%2==0) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
