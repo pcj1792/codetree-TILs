e917588f3b3ba13ca9ab4945ac070c361d65a5e1
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-11 10:52:43 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240311/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -3,9 +3,19 @@ using namespace std;
 int main() {
     int a, b ,sum=0;
     cin >> a >> b;
-    for(int i=a; i<=b; i++){
-        if(i%5==0){
+    if(a<=b){
+        for(int i=a; i<=b; i++){
+            if(i%5==0){
             sum+=i;
+            }
+        }
+    }
+    
+    else{
+        for(int i=b; i<=a; i++){
+            if(i%5==0){
+            sum+=i;
+            }
         }
     }
     cout << sum;
