2a01db9dc5b78526021381b5e433d525837d9638
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-11 18:28:05 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240311/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int a,b,m;
+    cin >> a >> b;
+    for(int i = a; i<=b; i++){
+        m=pow(a,b);
+    }
+    cout << m;
+    return 0;
+}
