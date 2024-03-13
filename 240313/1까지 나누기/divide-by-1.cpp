4b9d0a5ab2428eec3de9ae0e365ef5c8874fb668
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-13 09:53:36 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240313/1까지 나누기/divide-by-1.cpp
+++ 240313/1까지 나누기/divide-by-1.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int n, cnt=1;
+    int n, cnt = 0; 
     cin >> n;
-    for(int i =1; i<=n; i++){
+
+    for (int i = 1; i <= 5000; i++) {
+        n /= i; 
         cnt++;
-        n/=i;
-        
-        if(n/i<=1) {
+        if (n <= 1) {
             break;
         }
     }
+
     cout << cnt;
     return 0;
 }
