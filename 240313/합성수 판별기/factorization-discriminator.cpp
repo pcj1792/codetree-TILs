30836ca641f6abe1f178fb55d5a04c4d52e74e95
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2024-03-13 17:32:30 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240313/합성수 판별기/factorization-discriminator.cpp
+++ 240313/합성수 판별기/factorization-discriminator.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 int main() {
     int n;
-    bool a=false;
     cin >> n;
+    bool a=false;
+
     for(int i=2; i<n; i++){
         if(n%i==0){
             a=true;    
         }
     }
+
     if(a==true) cout << 'C';
+    
     else cout << 'N';
     return 0;
 }
