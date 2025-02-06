be6b55fd0ece3ce9b29887b477406ea04e4a5e1c
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2025-02-06 20:34:09 +0900

    Title: 별표 출력하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250206/별표 출력하기/print-star.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+
+for k in range(n):
+    for l in range(n-k-1):
+        print('*', end=' ')
+    print()
