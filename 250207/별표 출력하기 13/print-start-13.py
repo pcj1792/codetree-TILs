5b22623ee18c1c6785348b980f5589754e5ddcf9
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2025-02-07 13:07:38 +0900

    Title: 별표 출력하기 13, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250207/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(2*n):
+    if i % 2 == 0:
+        for _ in range(n-i//2):
+            print('*', end=" ")
+        print()
+    
+    else:
+        for _ in range(i//2+1):
+            print('*', end=" ")
+        print()        
