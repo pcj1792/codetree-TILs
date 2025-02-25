5b91541f830c852931d72aec2cefc053491ff7ff
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2025-02-25 14:40:03 +0900

    Title: 배열의 특정 위치 찾기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250225/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 250225/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -1,6 +1,6 @@
 nums = list(map(int,input().split()))
 
 ood_sum = sum(nums[1::2])
-multi_avr = sum([x for x in nums if x % 3 == 0]) /3
+multi_avr = sum([x for x in nums if x % 3 == 0]) / 3
 
-print(ood_sum,f'{multi_avr:.1f}')
+print(ood_sum,f'{multi_avr:.1f}', sep=" ")
