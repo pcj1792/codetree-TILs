26706f501ccbbccc4ff4c1bacdb64fe9000c9bda
pcj1792 <154817102+pcj1792@users.noreply.github.com>
2025-02-27 16:19:07 +0900

    Title: 십의 자리 숫자, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/십의 자리 숫자/number-of-tens-digit.py
+++ 250227/십의 자리 숫자/number-of-tens-digit.py
@@ -2,11 +2,12 @@ arr = list(map(int,input().split()))
 
 cnt_arr = [0]*10
 for i in arr:
-    if i >= 10:
+    if i >= 10 :
         cnt_arr[i//10] += 1
-    else:
-        pass
+    if i == 0:
+        break
+        
 
 
-for i in range(1,len(arr)):
+for i in range(1,len(cnt_arr)):
     print(f'{i} - {cnt_arr[i]}')
